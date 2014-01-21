43bc22673b3403777c360392fa1517ac1adf5efc
revir <revir.qing@gmail.com>
2014-01-21 08:20:55 +0800

    add dict images

--- core.js
+++ core.js
@@ -20,10 +20,10 @@
 
 function setBrowserIcon(enable) {
     var title = '已打开鼠标取词功能',
-        imgPath = 'images/icon-on19.png';
+        imgPath = 'images/dict-on24.png';
     if (!enable) {
         title = '已关闭鼠标取词功能';
-        imgPath = 'images/icon-off19.png';
+        imgPath = 'images/dict-off24.png';
     }
     chrome.browserAction.setTitle({
         title: title
