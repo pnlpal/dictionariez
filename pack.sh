60bd47440e0515303aa956f5df0af4b9a3558ad5
river <revir.qing@gmail.com>
2019-11-05 21:13:22 +0800

    update: pack.sh to exclude node_modules

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 cd ..
 rm -f FairyDict.zip
-zip -x '*.DS_Store*' -x '*build/*' -x '*readme_images/*' -x '*.git*' -x '*/test/*' -x '*.less' -x '*.coffee' -x '*.scss' -r FairyDict.zip  FairyDict/
+zip -x '*.DS_Store*' -x '*build/*' -x '*readme_images/*' -x '*node_modules/*' -x '*.git*' -x '*/test/*' -x '*.less' -x '*.coffee' -x '*.scss' -r FairyDict.zip  FairyDict/
