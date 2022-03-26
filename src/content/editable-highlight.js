a077d01d64002fba483ede47a5896af07d55cc56
River Yang <river.yang@pinmeto.com>
2022-03-26 16:10:35 +0100

    Dictionaries -> Dictionariez

--- src/content/editable-highlight.js
+++ src/content/editable-highlight.js
@@ -1,23 +1,23 @@
 const ignoredSites = [
-    "https://twitter.com", // https://github.com/pnlpal/dictionaries/issues/38
+  "https://twitter.com", // https://github.com/pnlpal/dictionariez/issues/38
 ];
 
 export default function (colour) {
-    if (ignoredSites.includes(location.origin)) 
-        return; 
+  if (ignoredSites.includes(location.origin)) return;
 
-    var range, sel = window.getSelection();
-    if (sel.rangeCount && sel.getRangeAt) {
-        range = sel.getRangeAt(0);
-    };
-    document.designMode = "on";
-    if (range) {
-        sel.removeAllRanges();
-        sel.addRange(range);
-    };
-    // Use HiliteColor since some browsers apply BackColor to the whole block
-    if (!document.execCommand("HiliteColor", false, colour)) {
-        document.execCommand("BackColor", false, colour);
-    };
-    document.designMode = "off";
-};
+  var range,
+    sel = window.getSelection();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
+  }
+  document.designMode = "on";
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  // Use HiliteColor since some browsers apply BackColor to the whole block
+  if (!document.execCommand("HiliteColor", false, colour)) {
+    document.execCommand("BackColor", false, colour);
+  }
+  document.designMode = "off";
+}
