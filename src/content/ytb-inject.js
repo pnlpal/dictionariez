188bcbba87f6ffd4cdee9ba15319c04fadab3344
River <revir.qing@gmail.com>
2023-12-31 10:53:11 +0100

    be able to get video info

--- src/content/ytb-inject.js
+++ src/content/ytb-inject.js
@@ -10,13 +10,14 @@ export function initYtbInjection() {
     return xml.trim();
   }
 
-  async function getCaptionsFromVideo(info) {
+  async function getVideoCaptions(info) {
     if (!info.captions) {
       console.error(
         "[ytb-inject] There are restrictions preventing you from viewing this video"
       );
       return;
     }
+    // console.log("video info", info);
     try {
       var captions =
         info.captions.playerCaptionsTracklistRenderer.captionTracks;
@@ -49,27 +50,46 @@ export function initYtbInjection() {
     return result;
   }
 
-  async function getCaptions(videoLink = "") {
+  function getVideoInfo(info) {
+    if (!info.videoDetails) {
+      console.error(
+        "[ytb-inject] There are restrictions preventing you from getting info of this video"
+      );
+      return;
+    }
+
+    return {
+      snapshot:
+        "https://i.ytimg.com/vi/" +
+        info.videoDetails.videoId +
+        "/hqdefault.jpg",
+      videoId: info.videoDetails.videoId,
+      title: info.videoDetails.title,
+      shortDescription: info.videoDetails.shortDescription,
+    };
+  }
+
+  async function parse(videoLink = "") {
     if (typeof getPageData == "function") {
       var info = getPageData().data.playerResponse;
       if (info && info != "undefined") {
-        return getCaptionsFromVideo(info);
+        return info;
       }
     } else if (typeof window.getPageData == "function") {
       var info = getPageData().data.playerResponse;
       if (info && info != "undefined") {
-        return getCaptionsFromVideo(info);
+        return info;
       }
     }
     if (typeof ytInitialPlayerResponse != "undefined") {
       var info = ytInitialPlayerResponse;
       if (info && info != "undefined") {
-        return getCaptionsFromVideo(info);
+        return info;
       }
     } else if (typeof window.ytInitialPlayerResponse != "undefined") {
       var info = window.ytInitialPlayerResponse;
       if (info && info != "undefined") {
-        return getCaptionsFromVideo(info);
+        return info;
       }
     }
     if (videoLink) {
@@ -86,9 +106,7 @@ export function initYtbInjection() {
               "var ytInitialPlayerResponse = "
           )[1]
           .split("</script>")[0];
-        return getCaptionsFromVideo(
-          eval("(function() {return " + scriptPt1 + "})();")
-        );
+        return eval("(function() {return " + scriptPt1 + "})();");
       } catch (e) {
         console.error("[ytb-inject]", e);
         return;
@@ -96,23 +114,30 @@ export function initYtbInjection() {
     }
   }
 
-  window.addEventListener("message", (event) => {
+  window.addEventListener("message", async (event) => {
     if (
       event.data &&
-      event.data.type === "getCaptions" &&
+      (event.data.type === "getCaptions" ||
+        event.data.type === "getVideoInfo") &&
       ["http://localhost:4200", "https://pnlpal.dev"].includes(event.origin)
     ) {
-      //   console.log("event", event.data, event.origin);
-      getCaptions(event.data.videoLink).then((captions) => {
-        window.parent.postMessage(
-          {
-            type: "captions",
-            captions: captions,
-            videoId: event.data.videoId,
-          },
-          event.origin
-        );
-      });
+      // console.log("event", event.data, event.origin);
+      const videoData = await parse(event.data.videoLink);
+      // console.log("parsed video data", videoData);
+      const response = {
+        type: "captions",
+        videoId: event.data.videoId,
+      };
+      if (!event.data.noCaptions) {
+        response.captions = await getVideoCaptions(videoData);
+        // console.log("Got captions:", response.captions.length);
+      }
+      if (!event.data.noVideoInfo) {
+        response.videoInfo = getVideoInfo(videoData);
+        // console.log("Got videoInfo: ", response.videoInfo);
+      }
+
+      window.parent.postMessage(response, event.origin);
     }
   });
 }
