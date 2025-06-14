101b16fe0c51eb9cf6f6c6bc69d622f3e07379b1
River <revir.qing@gmail.com>
2025-06-14 21:03:22 +0200

    Revert "remove captionz"
    This reverts commit 4ff2433029965e408c072ef22a56ed2207c37c53.

---
+++ src/background/pnlpal.coffee
@@ -0,0 +1,21 @@
+import message from "./message.js"
+import setting from "./setting.js"
+
+
+openYtbOnCaptionz = (link) ->
+    url = "https://pnl.dev/captionz?link="+encodeURIComponent(link)
+
+    chrome.tabs.create {
+        url
+    }
+
+
+export default {
+    openYtbOnCaptionz,
+    init: () ->
+        message.on 'setting of ytb captionz', () ->
+            return { disableYtbCaptionz: setting.getValue("disableYtbCaptionz") }
+        message.on 'open ytb video on captionz', ({ link })->
+            if link.startsWith "https://www.youtube.com/watch"
+                openYtbOnCaptionz link 
+}
---
+++ src/content/ytb-inject.js
@@ -0,0 +1,152 @@
+export function initYtbInjection() {
+  if (location.hostname !== "www.youtube.com" || window.self === window.top)
+    return;
+
+  // console.log("Injected in ytb: ", location.href);
+
+  async function fetchCaption(url) {
+    var xml = await fetch(url);
+    xml = await xml.text();
+    return xml.trim();
+  }
+
+  async function getVideoCaptions(info) {
+    if (!info.captions) {
+      console.error(
+        "[ytb-inject] There are restrictions preventing you from viewing this video"
+      );
+      return;
+    }
+    // console.log("video info", info);
+    try {
+      var captions =
+        info.captions.playerCaptionsTracklistRenderer.captionTracks;
+
+      if (!captions) {
+        console.error("[ytb-inject] There is no caption from this video");
+        return;
+      }
+
+      if (captions.url) captions = [captions];
+    } catch (e) {
+      console.error("[ytb-inject]", e);
+      return;
+    }
+
+    const result = [];
+    for (const caption of captions) {
+      const item = {
+        videoId: info.videoDetails.videoId,
+        languageName: caption.name.simpleText,
+        languageCode: caption.languageCode,
+        xml: await fetchCaption(caption.baseUrl),
+      };
+
+      result.push(item);
+    }
+
+    return result;
+  }
+
+  function getVideoInfo(info) {
+    if (!info.videoDetails) {
+      console.error(
+        "[ytb-inject] There are restrictions preventing you from getting info of this video"
+      );
+      return;
+    }
+
+    return {
+      snapshot:
+        "https://i.ytimg.com/vi/" +
+        info.videoDetails.videoId +
+        "/hqdefault.jpg",
+      videoId: info.videoDetails.videoId,
+      title: info.videoDetails.title,
+      shortDescription: info.videoDetails.shortDescription,
+    };
+  }
+
+  async function parse(videoLink = "") {
+    if (typeof getPageData == "function") {
+      var info = getPageData().data.playerResponse;
+      if (info && info != "undefined") {
+        return info;
+      }
+    } else if (typeof window.getPageData == "function") {
+      var info = getPageData().data.playerResponse;
+      if (info && info != "undefined") {
+        return info;
+      }
+    }
+    if (typeof ytInitialPlayerResponse != "undefined") {
+      var info = ytInitialPlayerResponse;
+      if (info && info != "undefined") {
+        return info;
+      }
+    } else if (typeof window.ytInitialPlayerResponse != "undefined") {
+      var info = window.ytInitialPlayerResponse;
+      if (info && info != "undefined") {
+        return info;
+      }
+    }
+    if (videoLink) {
+      try {
+        var response = await window.fetch(videoLink);
+        var body = await response.text();
+        var scriptPt1 = body
+          .split(
+            "<script" +
+              body
+                .split("var ytInitialPlayerResponse = ")[0]
+                .split("<script")
+                .pop() +
+              "var ytInitialPlayerResponse = "
+          )[1]
+          .split("</script>")[0];
+        return scriptPt1;
+      } catch (e) {
+        console.error("[ytb-inject]", e);
+        return;
+      }
+    }
+  }
+
+  window.addEventListener("message", async (event) => {
+    if (
+      event.data &&
+      [
+        "http://localhost:4100",
+        "https://pnlpal.dev",
+        "https://pnl.dev",
+      ].includes(event.origin)
+    ) {
+      // console.log("event", event.data, event.origin);
+      if (event.data.getRawVideoDataFirst) {
+        const videoData = await parse(event.data.videoLink);
+        window.parent.postMessage(
+          { videoData, videoId: event.data.videoId, type: "rawVideoData" },
+          event.origin
+        );
+      } else if (
+        event.data.type === "getCaptions" ||
+        event.data.type === "getVideoInfo"
+      ) {
+        // console.log("parsed video data", videoData);
+        const response = {
+          type: "captions",
+          videoId: event.data.videoId,
+        };
+        if (!event.data.noCaptions) {
+          response.captions = await getVideoCaptions(event.data.videoData);
+          // console.log("Got captions:", response.captions.length);
+        }
+        if (!event.data.noVideoInfo) {
+          response.videoInfo = getVideoInfo(event.data.videoData);
+          // console.log("Got videoInfo: ", response.videoInfo);
+        }
+        window.parent.postMessage(response, event.origin);
+      }
+    }
+  });
+}
