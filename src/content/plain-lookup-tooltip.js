79ab1ea821fc0f222ec789e12e6a68fa3119d7f5
River <revir.qing@gmail.com>
2025-11-09 10:30:12 +0100

    just add language tooltip for symbol

--- src/content/plain-lookup-tooltip.js
+++ src/content/plain-lookup-tooltip.js
@@ -2,44 +2,44 @@ import utils from "utils";
 import debounce from "lodash/debounce";
 
 const setupAudioListener = () => {
-  $(document).on(
-    "click mouseover",
-    ".fairydict-pron-audio",
-    debounce(
-      (e) => {
-        e.stopPropagation();
+    $(document).on(
+        "click mouseover",
+        ".fairydict-pron-audio",
+        debounce(
+            (e) => {
+                e.stopPropagation();
 
-        if (utils.isMobile() && e.type === "mouseover") {
-          return; // on mobile mouseover will be triggered when click.
-        }
+                if (utils.isMobile() && e.type === "mouseover") {
+                    return; // on mobile mouseover will be triggered when click.
+                }
 
-        let synthesisObj = null;
+                let synthesisObj = null;
 
-        if ($(e.currentTarget).data("mp3")) {
-          utils.send("play audios", {
-            otherSrc: $(e.currentTarget).data("mp3"),
-          });
-        } else if ($(e.currentTarget).data("synthesis")) {
-          synthesisObj = {
-            text: $(e.currentTarget).data("w"),
-            name: $(e.currentTarget).data("lang"),
-            lang: $(e.currentTarget).data("synthesis"),
-          };
-          utils.send("play audios", { synthesisObj });
-        } else if ($(e.currentTarget).hasClass("for-chatgpt-audio")) {
-          synthesisObj = {
-            text: $(e.currentTarget).parent().text(),
-            lang: "en-US",
-          };
-          utils.send("play audios", { synthesisObj });
-        }
+                if ($(e.currentTarget).data("mp3")) {
+                    utils.send("play audios", {
+                        otherSrc: $(e.currentTarget).data("mp3"),
+                    });
+                } else if ($(e.currentTarget).data("synthesis")) {
+                    synthesisObj = {
+                        text: $(e.currentTarget).data("w"),
+                        name: $(e.currentTarget).data("lang"),
+                        lang: $(e.currentTarget).data("synthesis"),
+                    };
+                    utils.send("play audios", { synthesisObj });
+                } else if ($(e.currentTarget).hasClass("for-chatgpt-audio")) {
+                    synthesisObj = {
+                        text: $(e.currentTarget).parent().text(),
+                        lang: "en-US",
+                    };
+                    utils.send("play audios", { synthesisObj });
+                }
 
-        return false;
-      },
-      1000,
-      { leading: true, trailing: false }
-    )
-  );
+                return false;
+            },
+            1000,
+            { leading: true, trailing: false }
+        )
+    );
 };
 
 const wTpl = (w) => `<strong class='fairydict-w'> ${w} </strong>`;
@@ -48,116 +48,94 @@ const defsTpl = (defs) => `<span class='fairydict-defs'> ${defs} </span>`;
 const labelsTpl = (labels) => `<div class='fairydict-labels'> ${labels} </div>`;
 const labelTpl = (label) => `<span class='fairydict-label'> ${label} </span>`;
 const posTpl = (pos) => `<span class='fairydict-pos'> ${pos} </span>`;
-const contentTpl = (content) =>
-  `<div class='fairydict-content'> ${content} </div>`;
-const pronSymbolTpl = (symbol = "", type = "") =>
-  `<span class='fairydict-symbol fairydict-symbol-${type}'> <em> ${symbol} </em> </span>`;
+const contentTpl = (content) => `<div class='fairydict-content'> ${content} </div>`;
+const pronSymbolTpl = (symbol = "", type = "", lang = "") =>
+    `<span class='fairydict-symbol fairydict-symbol-${type}' title='${lang}'> <em> ${symbol} </em> </span>`;
 const pronAudioTpl = (w, src = "", type = "", synthesis = "", lang = "") =>
-  `<a class='fairydict-pron-audio fairydict-pron-audio-${type}' data-mp3='${src}' data-synthesis='${synthesis}' data-lang='${lang}' data-w='${w}'><i class='icon-fairydict-volume'></i></a>`;
-const pronsTpl = (w, prons) =>
-  `<div class='fairydict-prons'> ${w} ${prons} </div>`;
+    `<a class='fairydict-pron-audio fairydict-pron-audio-${type}' data-mp3='${src}' data-synthesis='${synthesis}' data-lang='${lang}' data-w='${w}'><i class='icon-fairydict-volume'></i></a>`;
+const pronsTpl = (w, prons) => `<div class='fairydict-prons'> ${w} ${prons} </div>`;
 
 const renderQueryResult = (res) => {
-  let html = "";
+    let html = "";
 
-  let wHtml = "";
-  let pronHtml = "";
-  if (res?.w) {
-    wHtml = wTpl(res.w);
+    let wHtml = "";
+    let pronHtml = "";
+    if (res?.w) {
+        wHtml = wTpl(res.w);
 
-    if (res?.prons) {
-      pronHtml = res.prons.reduce((prev, cur) => {
-        if (cur.synthesis || cur.audio || cur.symbol) {
-          if (cur.symbol) prev += pronSymbolTpl(cur.symbol, cur.type);
-          if (cur.synthesis || cur.audio)
-            prev += pronAudioTpl(
-              res.w.replaceAll("·", ""),
-              cur.audio,
-              cur.type,
-              cur.synthesis,
-              res.lang
-            );
+        if (res?.prons) {
+            pronHtml = res.prons.reduce((prev, cur) => {
+                if (cur.synthesis || cur.audio || cur.symbol) {
+                    if (cur.symbol) prev += pronSymbolTpl(cur.symbol, cur.type, res.lang);
+                    if (cur.synthesis || cur.audio)
+                        prev += pronAudioTpl(res.w.replaceAll("·", ""), cur.audio, cur.type, cur.synthesis, res.lang);
+                }
+                return prev;
+            }, "");
         }
-        return prev;
-      }, "");
     }
-  }
 
-  if (pronHtml || wHtml) html += pronsTpl(wHtml, pronHtml);
+    if (pronHtml || wHtml) html += pronsTpl(wHtml, pronHtml);
 
-  const renderItem = (item) => {
-    let posHtml = "";
-    let defsHtml = "";
-    let labelsHtml = "";
+    const renderItem = (item) => {
+        let posHtml = "";
+        let defsHtml = "";
+        let labelsHtml = "";
 
-    if (item.pos) posHtml = posTpl(item.pos);
-    const labelsCon = (item.labels || [])
-      .map((name) => (name ? labelTpl(name) : ""))
-      .reduce((prev, cur) => (cur ? prev + cur : prev), "");
-    if (labelsCon) labelsHtml = labelsTpl(labelsCon);
-    const defs = Array.isArray(item.def) ? item.def : [item.def];
-    const defsCon = defs
-      .map((def, i) =>
-        def
-          ? defs.length === 1
-            ? defTpl(def)
-            : defTpl(`${i + 1}. ${def}`)
-          : ""
-      )
-      .reduce((prev, next) => (next ? prev + "<br>" + next : prev));
-    if (defsCon) defsHtml = defsTpl(defsCon);
+        if (item.pos) posHtml = posTpl(item.pos);
+        const labelsCon = (item.labels || [])
+            .map((name) => (name ? labelTpl(name) : ""))
+            .reduce((prev, cur) => (cur ? prev + cur : prev), "");
+        if (labelsCon) labelsHtml = labelsTpl(labelsCon);
+        const defs = Array.isArray(item.def) ? item.def : [item.def];
+        const defsCon = defs
+            .map((def, i) => (def ? (defs.length === 1 ? defTpl(def) : defTpl(`${i + 1}. ${def}`)) : ""))
+            .reduce((prev, next) => (next ? prev + "<br>" + next : prev));
+        if (defsCon) defsHtml = defsTpl(defsCon);
 
-    if (defsHtml) html += contentTpl(posHtml + labelsHtml + defsHtml);
-  };
+        if (defsHtml) html += contentTpl(posHtml + labelsHtml + defsHtml);
+    };
 
-  if (res?.defs) res.defs.forEach(renderItem);
-  if (res?.defs2) res.defs2.forEach(renderItem);
-  return html;
+    if (res?.defs) res.defs.forEach(renderItem);
+    if (res?.defs2) res.defs2.forEach(renderItem);
+    return html;
 };
 
 const getEnglishPronAudio = async (w) => {
-  const res = await utils.send("get real person voice", { w });
-  if (res?.prons) {
-    let ameSrc = "";
-    let breSrc = "";
-    function addPronAudio(item) {
-      if ($(`.fairydict-pron-audio-${item.type}`).length) {
-        $(`.fairydict-pron-audio-${item.type}`).attr("data-mp3", item.audio);
-      } else {
-        const audioEl = pronAudioTpl(
-          w.replaceAll("·", ""),
-          item.audio,
-          item.type,
-          item.synthesis,
-          "English"
-        );
-        const label_ =
-          item.type === "ame" ? "US" : item.type === "bre" ? "UK" : item.type;
-        const elWithLabel = `<em>${label_}</em>${audioEl}`;
-        $(".fairydict-prons").append(elWithLabel);
-      }
+    const res = await utils.send("get real person voice", { w });
+    if (res?.prons) {
+        let ameSrc = "";
+        let breSrc = "";
+        function addPronAudio(item) {
+            if ($(`.fairydict-pron-audio-${item.type}`).length) {
+                $(`.fairydict-pron-audio-${item.type}`).attr("data-mp3", item.audio);
+            } else {
+                const audioEl = pronAudioTpl(w.replaceAll("·", ""), item.audio, item.type, item.synthesis, "English");
+                const label_ = item.type === "ame" ? "US" : item.type === "bre" ? "UK" : item.type;
+                const elWithLabel = `<em>${label_}</em>${audioEl}`;
+                $(".fairydict-prons").append(elWithLabel);
+            }
+        }
+        for (const item of res.prons) {
+            addPronAudio(item);
+            if (item.type === "ame") {
+                ameSrc = item.audio;
+            } else if (item.type === "bre") {
+                breSrc = item.audio;
+            }
+        }
+        utils.send("play audios", { ameSrc, breSrc, checkSetting: true });
     }
-    for (const item of res.prons) {
-      addPronAudio(item);
-      if (item.type === "ame") {
-        ameSrc = item.audio;
-      } else if (item.type === "bre") {
-        breSrc = item.audio;
-      }
-    }
-    utils.send("play audios", { ameSrc, breSrc, checkSetting: true });
-  }
 };
 
 export default {
-  $dictionariezTooltipContainer: null,
-  init() {
-    this.$dictionariezTooltipContainer = $("#dictionariez-tooltip-container")
-      .length
-      ? $("#dictionariez-tooltip-container")
-      : $("html");
+    $dictionariezTooltipContainer: null,
+    init() {
+        this.$dictionariezTooltipContainer = $("#dictionariez-tooltip-container").length
+            ? $("#dictionariez-tooltip-container")
+            : $("html");
 
-    $(`
+        $(`
 <div class="dictionaries-tooltip">
     <div class="fairydict-spinner">
     <div class="fairydict-bounce1"></div>
@@ -169,108 +147,102 @@ export default {
 </div>
 `).appendTo(this.$dictionariezTooltipContainer);
 
-    setupAudioListener();
-  },
-  setupPlainContentPosition(e) {
-    const $el = $(".dictionaries-tooltip");
-    let pageX, pageY;
+        setupAudioListener();
+    },
+    setupPlainContentPosition(e) {
+        const $el = $(".dictionaries-tooltip");
+        let pageX, pageY;
 
-    if (e.pageX && e.pageY) {
-      pageX = e.pageX;
-      pageY = e.pageY;
-    } else {
-      pageX = e.changedTouches ? e.changedTouches[0].pageX : 0;
-      pageY = e.changedTouches ? e.changedTouches[0].pageY : 0;
-    }
-
-    const containerOffset = this.$dictionariezTooltipContainer.offset();
-    if ($el.length && pageY) {
-      let mousex =
-        pageX -
-        containerOffset.left +
-        25 +
-        (this.$dictionariezTooltipContainer.data(
-          "dictionariezTooltipOffsetLeft"
-        ) || 0);
-      let mousey =
-        pageY -
-        containerOffset.top +
-        25 +
-        (this.$dictionariezTooltipContainer.data(
-          "dictionariezTooltipOffsetTop"
-        ) || 0);
-      let top = mousey;
-      let left = mousex;
-
-      const rect = $("#dictionariez-tooltip-container").length
-        ? $("#dictionariez-tooltip-container")[0].getBoundingClientRect()
-        : window.document.scrollingElement.getBoundingClientRect();
-      const domW = window.innerWidth - rect.left;
-      const domH = window.innerHeight - rect.top;
-
-      const isOnSmallScreen = domW < 600;
-
-      if (domW - left < 300) {
-        left = domW - 300;
-      }
-      if (domH - top < 200) {
-        top = top - 10;
-      }
-      if (domH - top < 30) {
-        top = top - 100;
-      }
-
-      if (isOnSmallScreen) {
-        $el.css({
-          top,
-          left: "10px",
-          right: "10px",
-        });
-      } else {
-        $el.css({ top, left, marginRight: "10px" });
-      }
-    }
-  },
-  showPlainContent(htmlContent, e) {
-    $(".dictionaries-tooltip").fadeIn("slow");
-    if (htmlContent) {
-      $(".dictionaries-tooltip .fairydict-spinner").hide();
-      $(".dictionaries-tooltip .dictionaries-tooltip-content").append(
-        htmlContent
-      );
-    } else {
-      const clickInside = $(".dictionaries-tooltip").has(e.target).length;
-      $(".dictionaries-tooltip .fairydict-spinner").show();
-      $(".dictionaries-tooltip .dictionaries-tooltip-content").empty();
-
-      if (e) {
-        if (!clickInside) {
-          this.setupPlainContentPosition(e);
+        if (e.pageX && e.pageY) {
+            pageX = e.pageX;
+            pageY = e.pageY;
+        } else {
+            pageX = e.changedTouches ? e.changedTouches[0].pageX : 0;
+            pageY = e.changedTouches ? e.changedTouches[0].pageY : 0;
         }
-      }
-    }
-  },
-  hide() {
-    $(".dictionaries-tooltip").fadeOut().hide();
-  },
-  renderPlainResult(res) {
-    let html = "";
-    res = Array.isArray(res) ? res : [res];
-    for (const item of res) {
-      html += renderQueryResult(item);
 
-      if (item?.prons?.length && item.w) {
-        if (item.prons.some((v) => ["bre", "ame"].includes(v.type))) {
-          getEnglishPronAudio(item.w);
+        const containerOffset = this.$dictionariezTooltipContainer.offset();
+        if ($el.length && pageY) {
+            let mousex =
+                pageX -
+                containerOffset.left +
+                25 +
+                (this.$dictionariezTooltipContainer.data("dictionariezTooltipOffsetLeft") || 0);
+            let mousey =
+                pageY -
+                containerOffset.top +
+                25 +
+                (this.$dictionariezTooltipContainer.data("dictionariezTooltipOffsetTop") || 0);
+            let top = mousey;
+            let left = mousex;
+
+            const rect = $("#dictionariez-tooltip-container").length
+                ? $("#dictionariez-tooltip-container")[0].getBoundingClientRect()
+                : window.document.scrollingElement.getBoundingClientRect();
+            const domW = window.innerWidth - rect.left;
+            const domH = window.innerHeight - rect.top;
+
+            const isOnSmallScreen = domW < 600;
+
+            if (domW - left < 300) {
+                left = domW - 300;
+            }
+            if (domH - top < 200) {
+                top = top - 10;
+            }
+            if (domH - top < 30) {
+                top = top - 100;
+            }
+
+            if (isOnSmallScreen) {
+                $el.css({
+                    top,
+                    left: "10px",
+                    right: "10px",
+                });
+            } else {
+                $el.css({ top, left, marginRight: "10px" });
+            }
         }
-      }
-    }
+    },
+    showPlainContent(htmlContent, e) {
+        $(".dictionaries-tooltip").fadeIn("slow");
+        if (htmlContent) {
+            $(".dictionaries-tooltip .fairydict-spinner").hide();
+            $(".dictionaries-tooltip .dictionaries-tooltip-content").append(htmlContent);
+        } else {
+            const clickInside = $(".dictionaries-tooltip").has(e.target).length;
+            $(".dictionaries-tooltip .fairydict-spinner").show();
+            $(".dictionaries-tooltip .dictionaries-tooltip-content").empty();
 
-    if (html) {
-      this.showPlainContent(html);
-    } else {
-      this.hide();
-    }
-    return html;
-  },
+            if (e) {
+                if (!clickInside) {
+                    this.setupPlainContentPosition(e);
+                }
+            }
+        }
+    },
+    hide() {
+        $(".dictionaries-tooltip").fadeOut().hide();
+    },
+    renderPlainResult(res) {
+        let html = "";
+        res = Array.isArray(res) ? res : [res];
+        for (const item of res) {
+            html += renderQueryResult(item);
+
+            if (item?.prons?.length && item.w) {
+                if (item.prons.some((v) => ["bre", "ame"].includes(v.type))) {
+                    getEnglishPronAudio(item.w);
+                }
+            }
+        }
+
+        if (html) {
+            this.showPlainContent(html);
+        } else {
+            this.hide();
+        }
+        return html;
+    },
 };
