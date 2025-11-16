b83c50ed1a91ed7a5759ca9331d13bdc02abfee4
River <revir.qing@gmail.com>
2025-11-16 21:11:04 +0100

    ignore clicks inside the player

--- src/content/tts-inject.js
+++ src/content/tts-inject.js
@@ -3,9 +3,18 @@ import debounce from "lodash/debounce";
 import { detectLanguage } from "../shared-readonly/detectLanguage.js";
 
 export default () => {
-    const handleSentenceSelected = async () => {
+    function isInPlayer(node) {
+        while (node) {
+            if (node.tagName === "PNL-TTS-PLAYER") {
+                return true;
+            }
+            node = node.parentNode;
+        }
+        return false;
+    }
+    const handleSentenceSelected = async (ev) => {
         const text = window.getSelection().toString().trim();
-        if (!text || !utils.isSentence(text)) {
+        if (!text || !utils.isSentence(text) || isInPlayer(ev.target)) {
             return;
         }
         const lang = (await detectLanguage(text)) || localStorage.getItem("pnl-tts-lang-detected");
