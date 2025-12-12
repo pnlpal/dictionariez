601143a4970ed87727d195adcfd31f0d45070e7e
River <revir.qing@gmail.com>
2025-12-12 12:05:44 +0100

    github links

--- landing-page/devserver.js
+++ landing-page/devserver.js
@@ -10,6 +10,13 @@ const PORT = 8080;
 app.use(express.static(__dirname));
 // Map root requests for .jpg/.png/.gif to ../readme_images
 app.use("/", express.static(path.join(__dirname, "../readme_images")));
+app.get("/github-badge.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../src/vendor/github-badge.js"));
+});
+// Serve favicon at /library-64.png
+app.get("/library-64.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "../src/images/library-64.png"));
+});
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Dictionariez landing page running at http://localhost:${PORT}`);
--- src/vendor/github-badge.js
+++ src/vendor/github-badge.js
@@ -7,29 +7,27 @@
 // @match        https://awesome-go.com/
 // @grant        none
 // ==/UserScript==
-import $ from "jquery";
 
 (function () {
     "use strict";
-    var gitLinks = $("a.link-github[href*='github.com']");
+    const gitLinks = document.querySelectorAll("a.link-github[href*='github.com']");
 
-    gitLinks.each(function () {
-        var a = $(this);
-        var link = a.attr("href");
-        var re = /github.com\/([^/]+)\/([^/]+)\/?/;
-        var matches = link.match(re);
+    gitLinks.forEach((a) => {
+        const link = a.href;
+        const re = /github.com\/([^/]+)\/([^/]+)\/?/;
+        const matches = link.match(re);
         if (!matches || matches.length < 3) {
             return;
         }
-        var user = matches[1] || "stars";
-        var repo = matches[2] || "badges";
-        var shield = "https://img.shields.io/github/stars/" + user + "/" + repo + ".svg";
+        const user = matches[1] || "stars";
+        const repo = matches[2] || "badges";
+        const shield = `https://img.shields.io/github/stars/${user}/${repo}.svg`;
 
-        var b = document.createElement("img");
-        b.style.margin = "0 0 0 5px";
-        b.style.padding = "0";
-        b.src = shield;
-        b.height = 20;
-        a.append(b);
+        const img = document.createElement("img");
+        img.style.margin = "0 0 0 5px";
+        img.style.padding = "0";
+        img.src = shield;
+        img.height = 20;
+        a.appendChild(img);
     });
 })();
