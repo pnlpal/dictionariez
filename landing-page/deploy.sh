f0fe682c5e592b3186a8e2293aebb937ca1c1279
River <revir.qing@gmail.com>
2025-12-22 18:45:05 +0100

    Stop tracking landing-page deploy artifacts

--- landing-page/deploy.sh
+++
@@ -1,5 +0,0 @@
-set -e # Exit on error
-
-scp -P 4500 ../src/vendor/github-badge.js ../readme_images/screenshot* ../readme_images/Dictionariez-promo-1280x640.png ../src/images/library-64.png *.js *.css *.json index.html river@167.172.46.156:~/pnlpal/dictionariez-landing-page
-
-ssh -p 4500 river@167.172.46.156 "sudo cp ~/pnlpal/dictionariez-landing-page/* /usr/share/nginx/html/dictionariez-landing-page -r"
